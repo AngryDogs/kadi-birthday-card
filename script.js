bcbefd7cfe6dc7faffe2fc8441b04a883958386e
Rain Vink <rain.vink@telia.ee>
2023-05-18 03:11:02 +0300

    Changes

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.addEventListener('click', (event) => {
-  bursty(event.pageX, event.pageY);
-});
-
-setInterval(() => randomizedConfetti(), 500);
-
 function bursty(x, y) {
   const burst = new mojs.Burst({
     left: 0,
@@ -23,27 +17,61 @@ function bursty(x, y) {
   burst.replay();
 }
 
-function randomizedConfetti() {
+document.addEventListener('click', (event) => {
+  bursty(event.pageX, event.pageY);
+});
+
+let confetti1 = new mojs.Burst({
+  left: 0,
+  top: 0,
+  radius: { 0: 200 },
+  count: 20,
+  degree: 360,
+  children: {
+    fill: { white: '#34E1FF' },
+    duration: 2000,
+  },
+});
+
+let confetti2 = new mojs.Burst({
+  left: 0,
+  top: 0,
+  radius: { 0: 150 },
+  count: 20,
+  degree: 360,
+  children: {
+    fill: { white: '#34E1FF' },
+    duration: 1500,
+  },
+});
+
+let confetti3 = new mojs.Burst({
+  left: 0,
+  top: 0,
+  radius: { 0: 100 },
+  count: 30,
+  degree: 360,
+  children: {
+    fill: { white: '#34E1FF' },
+    duration: 1700,
+  },
+});
+
+setInterval(() => randomizedConfetti(confetti1), 1000);
+setInterval(() => randomizedConfetti(confetti2), 900);
+setInterval(() => randomizedConfetti(confetti3), 1500);
+
+function randomizedConfetti(confetti) {
   let randomX = Math.floor(
-    Math.random() * (document.body.clientWidth - 300) + 0
+    Math.random() * (document.body.clientWidth - 100) + 0
   );
-  let randomY = Math.floor(Math.random() * (window.innerHeight - 300) + 0);
-  const burst = new mojs.Burst({
-    left: 0,
-    top: 0,
-    radius: { 0: 200 },
-    count: 20,
-    degree: 360,
-    children: {
-      fill: { white: '#34E1FF' },
-      duration: 2000,
-    },
-  }).tune({
+  let randomY = Math.floor(Math.random() * (window.innerHeight - 100) + 0);
+  confetti.tune({
     x: randomX,
     y: randomY,
   });
 
-  burst.replay();
+  confetti.replay();
 }
 
 Splitting();
